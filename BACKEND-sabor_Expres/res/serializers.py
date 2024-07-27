47f2074e8806113950e5ee9a04cca4ccf17a62cb
Jherson <jinca@unsa.edu.pe>
2024-07-27 13:20:15 -0500

    subiendo django

---
+++ BACKEND-sabor_Expres/res/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Orden, Pago, Producto, Carrito, DetalleCarrito, Cliente
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+
+class DetalleCarritoSerializer(serializers.ModelSerializer):
+    producto = ProductoSerializer()
+
+    class Meta:
+        model = DetalleCarrito
+        fields = '__all__'
+
+class CarritoSerializer(serializers.ModelSerializer):
+    detalles = DetalleCarritoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Carrito
+        fields = ['id', 'cliente', 'fecha_creacion', 'activo', 'detalles']
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+class OrdenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Orden
+        fields = '__all__'
+
+class ProductoCarritoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+class PagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pago
+        fields = ['id', 'orden', 'fecha', 'metodo', 'monto', 'estado']
+        
